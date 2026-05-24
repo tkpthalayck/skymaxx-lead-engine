950e7c67d2f7119d744912a1734cc8aa008d750e
tkpthalayck <40851525+tkpthalayck@users.noreply.github.com>
2026-05-24 07:10:05 +0400

    fix: urllib.parse import + always write file

--- graph_test.py
+++ graph_test.py
@@ -1,73 +1,78 @@
-import os, urllib.request, urllib.error, json, time
+import os, urllib.request, urllib.error, urllib.parse, json, time, traceback
 
-tenant = os.environ['AZURE_TENANT_ID']
-client = os.environ['AZURE_CLIENT_ID']
-secret = os.environ['AZURE_CLIENT_SECRET']
-mailbox = os.environ['MAILBOX_EMAIL']
+tenant = os.environ.get('AZURE_TENANT_ID', '')
+client = os.environ.get('AZURE_CLIENT_ID', '')
+secret = os.environ.get('AZURE_CLIENT_SECRET', '')
+mailbox = os.environ.get('MAILBOX_EMAIL', '')
 
 log = []
 def p(m): print(m, flush=True); log.append(str(m))
 
 p('=== Microsoft Graph API Test ===')
-p('Tenant: ' + tenant[:12] + '...')
-p('Client: ' + client[:12] + '...')
+p('Tenant len: ' + str(len(tenant)))
+p('Client len: ' + str(len(client)))
+p('Secret len: ' + str(len(secret)))
 p('Mailbox: ' + mailbox)
 p('')
 
-# Step 1: Get token
-p('Step 1: Getting OAuth token...')
-token_url = f'https://login.microsoftonline.com/{tenant}/oauth2/v2.0/token'
-body = urllib.parse.urlencode({
-    'client_id': client,
-    'client_secret': secret,
-    'scope': 'https://graph.microsoft.com/.default',
-    'grant_type': 'client_credentials',
-}).encode()
-req = urllib.request.Request(token_url, data=body, method='POST',
-    headers={'Content-Type': 'application/x-www-form-urlencoded'})
 try:
-    resp = urllib.request.urlopen(req, timeout=30)
-    tok_data = json.loads(resp.read())
-    access_token = tok_data['access_token']
-    p('  OK token acquired (len=' + str(len(access_token)) + ')')
-except urllib.error.HTTPError as e:
-    err = e.read().decode()
-    p('  FAIL ' + str(e.code) + ': ' + err[:400])
-    with open('graph_test.txt', 'w') as f: f.write(chr(10).join(log))
-    raise SystemExit(1)
+    p('Step 1: Getting OAuth token...')
+    token_url = 'https://login.microsoftonline.com/' + tenant + '/oauth2/v2.0/token'
+    body = urllib.parse.urlencode({
+        'client_id': client,
+        'client_secret': secret,
+        'scope': 'https://graph.microsoft.com/.default',
+        'grant_type': 'client_credentials',
+    }).encode()
+    req = urllib.request.Request(token_url, data=body, method='POST',
+        headers={'Content-Type': 'application/x-www-form-urlencoded'})
+    try:
+        resp = urllib.request.urlopen(req, timeout=30)
+        tok_data = json.loads(resp.read())
+        access_token = tok_data['access_token']
+        p('  OK token (len=' + str(len(access_token)) + ')')
+    except urllib.error.HTTPError as e:
+        err = e.read().decode()
+        p('  FAIL HTTP ' + str(e.code) + ': ' + err[:500])
+        raise
 
-# Step 2: Get mailbox info
-p('')
-p('Step 2: Checking mailbox access...')
-graph_url = f'https://graph.microsoft.com/v1.0/users/{mailbox}'
-req = urllib.request.Request(graph_url, headers={'Authorization': f'Bearer {access_token}'})
-try:
-    resp = urllib.request.urlopen(req, timeout=30)
-    user = json.loads(resp.read())
-    p('  OK ' + user.get('displayName','?') + ' / ' + user.get('mail','?'))
-except urllib.error.HTTPError as e:
-    err = e.read().decode()
-    p('  FAIL ' + str(e.code) + ': ' + err[:400])
+    p('')
+    p('Step 2: Mailbox lookup...')
+    graph_url = 'https://graph.microsoft.com/v1.0/users/' + mailbox
+    req = urllib.request.Request(graph_url, headers={'Authorization': 'Bearer ' + access_token})
+    try:
+        resp = urllib.request.urlopen(req, timeout=30)
+        user = json.loads(resp.read())
+        p('  OK ' + str(user.get('displayName','?')) + ' / ' + str(user.get('mail','?')))
+    except urllib.error.HTTPError as e:
+        err = e.read().decode()
+        p('  FAIL HTTP ' + str(e.code) + ': ' + err[:500])
+        raise
 
-# Step 3: Read recent messages
-p('')
-p('Step 3: Fetching 5 most recent messages...')
-msg_url = f'https://graph.microsoft.com/v1.0/users/{mailbox}/messages?$top=5&$select=from,subject,receivedDateTime,isRead'
-req = urllib.request.Request(msg_url, headers={'Authorization': f'Bearer {access_token}'})
-try:
-    resp = urllib.request.urlopen(req, timeout=30)
-    msgs = json.loads(resp.read()).get('value', [])
-    p('  OK Found ' + str(len(msgs)) + ' messages')
-    for i, m in enumerate(msgs, 1):
-        sender = m.get('from', {}).get('emailAddress', {}).get('address', '?')
-        subj = m.get('subject', '')[:60]
-        date = m.get('receivedDateTime', '')[:16]
-        read = 'read' if m.get('isRead') else 'NEW'
-        p(f'  {i}. [{read}] {date} | {sender} | {subj}')
-except urllib.error.HTTPError as e:
-    err = e.read().decode()
-    p('  FAIL ' + str(e.code) + ': ' + err[:400])
+    p('')
+    p('Step 3: Reading 10 most recent messages...')
+    msg_url = 'https://graph.microsoft.com/v1.0/users/' + mailbox + '/messages?$top=10&$select=from,subject,receivedDateTime,isRead'
+    req = urllib.request.Request(msg_url, headers={'Authorization': 'Bearer ' + access_token})
+    try:
+        resp = urllib.request.urlopen(req, timeout=30)
+        msgs = json.loads(resp.read()).get('value', [])
+        p('  OK Found ' + str(len(msgs)) + ' messages')
+        for i, m in enumerate(msgs, 1):
+            sender = m.get('from', {}).get('emailAddress', {}).get('address', '?')
+            subj = m.get('subject', '')[:60]
+            date = m.get('receivedDateTime', '')[:16]
+            read = 'read' if m.get('isRead') else 'NEW'
+            p('  ' + str(i) + '. [' + read + '] ' + date + ' | ' + sender + ' | ' + subj)
+    except urllib.error.HTTPError as e:
+        err = e.read().decode()
+        p('  FAIL HTTP ' + str(e.code) + ': ' + err[:500])
 
-p('')
-p('=== Test Complete ===')
+    p('')
+    p('=== SUCCESS ===')
+
+except Exception as e:
+    p('')
+    p('EXCEPTION: ' + traceback.format_exc())
+
 with open('graph_test.txt', 'w') as f: f.write(chr(10).join(log))
+print('File written')
